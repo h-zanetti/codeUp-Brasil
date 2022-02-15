5b8b889daaa419a1052d348c3d8a84d875608b58
Henrique Zanetti <henriaguiar99@gmail.com>
2022-02-15 00:59:04 -0300

    Integrado template

--- webdev/urls.py
+++ webdev/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', include('webdev.core.urls')),
+    path('', include('webdev.core.urls')),
 ]
