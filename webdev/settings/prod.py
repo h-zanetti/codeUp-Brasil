292aaa433cc2e22f6c3a5180b13d8c8a99d14322
Henrique Zanetti <agah@liberaction.com.br>
2022-02-18 22:21:54 +0000

    Criado settings de prod

---
+++ webdev/settings/prod.py
@@ -0,0 +1,25 @@
+from webdev.settings.base import *
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['www.codeupbrasil.com.br', 'codeupbrasil.com.br']
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+    }
+}
+
